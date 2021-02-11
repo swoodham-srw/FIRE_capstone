629dacdf60841baf2ab18a077a42a83ad23e987a
swoodham-srw <74734103+swoodham-srw@users.noreply.github.com>
2021-02-11 15:40:46 -0700

    Add files via upload

--- test code.py
+++ test code.py
@@ -4,8 +4,36 @@ import matplotlib.pyplot as plt
 import retrying
 import requests
 import os
+import time
+
 
 #opening tif file
+'''
 image_file = plt.imread("test_code.tif")
 plt.imshow(image_file, cmap='Blues')
-plt.show()
+plt.show()'''
+
+#some calcul
+#save calc in file
+#Put file in directory
+#loop directory
+
+directory = "/Users/stonewoodham/Documents/PycharmProjects/capstone/FIRE_capstone"
+for filename in os.listdir(directory):
+    if filename.endswith(".tif"):
+        image_file = plt.imread(filename)
+        plt.imshow(image_file, cmap='Blues')
+        plt.show()
+        time.sleep(1)
+        continue
+    else:
+        continue
+
+#after calcul
+#save calc in file
+#put file in directory
+#repeat loop for after
+
+#new calcul = delta NDVI
+#plot delta NDVI 
+#save new file as delta NDVI
